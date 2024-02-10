89477581f2d09f210f43a454f6347099abbf9698
Evgen <veejayspb@gmail.com>
2024-02-10 21:10:58 +0300

    [fix]: ExceptionTest.

--- test/unit/component/ExceptionTest.php
+++ test/unit/component/ExceptionTest.php
@@ -29,7 +29,7 @@ final class ExceptionTest extends TestCase
     {
         return new class extends Response
         {
-            protected function getBody(array $data): string
+            protected function getBody(): string
             {
                 return '';
             }
